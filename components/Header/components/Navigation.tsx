c7f2f7e467e137e6e0e9c14b04f29c5b170afb85
Marek Bombera <bomber.marek@gmail.com>
2025-02-14 19:35:40 +0100

    feat: render nav links from store courses

--- components/Header/components/Navigation.tsx
+++ components/Header/components/Navigation.tsx
@@ -1,24 +1,25 @@
+'use client'
 import { FC } from "react"
 import Link from "next/link";
 import styles from './Navigation.module.scss'
+import { useSelector } from "react-redux";
+import { RootState } from "@/modules/course/store";
 
 
 const Navigation:FC = () => {
+  const courses = useSelector((state: RootState) => state.homePage.courses);
+
   return (
-    <nav className={styles.nav}>
+  <nav className={styles.nav}>
     <ul>
       <li>
         <Link href="/">Home</Link>
       </li>
-      <li>
-        <Link href="/course/ten-days-of-javascript">10 days of JavaScript</Link>
+      {courses.map((course) => (
+      <li key={course}>
+        <Link href={`/course/${course}`}>{course}</Link>
       </li>
-      <li>
-        <Link href="/course//java">Java</Link>
-      </li>
-      <li>
-        <Link href="/course/free-code-camp">Free Code Camp</Link>
-      </li>
+      ))}
     </ul>
   </nav>
   )
