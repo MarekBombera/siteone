2e99af94870f4bedf21774b1edc58da40d29260f
Marek Bombera <bomber.marek@gmail.com>
2025-02-14 19:56:49 +0100

    feat: add error handling to course form submit

--- modules/courses/components/AddCourseForm/AddCourseForm.tsx
+++ modules/courses/components/AddCourseForm/AddCourseForm.tsx
@@ -1,20 +1,42 @@
 'use client'
 import { addCourse } from "@/modules/course/coursesActions";
+import { RootState } from "@/modules/course/store";
 import ApiService from "@/services/ApiService";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const AddCourseForm: React.FC = () => {
+  const [error, setError] = useState<string | null>(null);
+  const courses = useSelector((state: RootState) => state.homePage.courses);
   const dispatch = useDispatch()
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     const formData = new FormData(e.currentTarget);
     const playlistId = formData.get("playlistId") as string;
 
-    const api = new ApiService();
-    const response = await api.getSlug(playlistId);
-    const json = await response.json();
-
-    dispatch(addCourse(json));
+    try {
+      const api = new ApiService();
+      const response = await api.getSlug(playlistId);
+      const json = await response.json();
+  
+      if (json.message) {
+        setError(json.message)
+        return
+      }
+      if (courses.includes(json)) {
+        setError('Course already exists')
+        return
+      }
+      if (error) {
+        setError(null)
+      }
+  
+      dispatch(addCourse(json));
+    }
+    catch {
+      setError('Failed to add course')
+      return
+    }
   }
 
   return (
@@ -28,6 +50,7 @@ const AddCourseForm: React.FC = () => {
         <input type="text" name="playlistId" />
       </label>
       <button type="submit">Add course</button>
+      {error ? <p>{error}</p> : null}
     </form>
   );
 };
